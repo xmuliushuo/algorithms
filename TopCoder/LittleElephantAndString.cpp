903691ad3d2438454ac3732839916c69f41179a7
xmuliushuo <xmuliushuo@gmail.com>
2013-11-21 11:01:11 +0800

    整理了代码

--- TopCoder/LittleElephantAndString.cpp
+++ TopCoder/LittleElephantAndString.cpp
@@ -1,3 +1,72 @@
+
+// Problem Statement
+//     
+// Little Elephant from the Zoo of Lviv likes strings.
+
+// You are given a string A and a string B of the same length. In one turn
+// Little Elephant can choose any character of A and move it to the beginning of
+// the string (i.e., before the first character of A). Return the minimal number
+// of turns needed to transform A into B. If it's impossible, return -1 instead.
+
+// Definition
+//     
+// Class: LittleElephantAndString
+// Method: getNumber
+// Parameters: string, string
+// Returns: int
+// Method signature: int getNumber(string A, string B)
+// (be sure your method is public)
+//     
+
+// Constraints
+// A will contain between 1 and 50 characters, inclusive.
+// B will contain between 1 and 50 characters, inclusive.
+// A and B will be of the same length.
+// A and B will consist of uppercase letters ('A'-'Z') only.
+
+// Examples
+
+// 0)
+//     
+// "ABC"
+// "CBA"
+// Returns: 2
+
+// The optimal solution is to make two turns. On the first turn, choose
+// character 'B' and obtain string "BAC". On the second turn, choose character
+// 'C' and obtain "CBA".
+
+// 1)
+//     
+// "A"
+// "B"
+// Returns: -1
+// In this case, it's impossible to transform A into B.
+
+// 2)
+//     
+// "AAABBB"
+// "BBBAAA"
+// Returns: 3
+
+// 3)
+//     
+// "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// "ZYXWVUTSRQPONMLKJIHGFEDCBA"
+// Returns: 25
+
+// 4)
+//     
+// "A"
+// "A"
+// Returns: 0
+
+// 5)
+//     
+// "DCABA"
+// "DACBA"
+// Returns: 2
+
 #include <vector>
 #include <string>
 #include <map>
